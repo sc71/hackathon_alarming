7e7a1962c5de8c0476fa40eb2d2ac08ec69476e7
Sarah Chen <scart11235@gmail.com>
2023-04-15 20:06:46 -0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import { ChromeMessage, Sender } from "./types";
 import './App.css';
+import { query } from 'express';
 
 function App() {
   const[url, setUrl] = useState("");
+  const [responseFromContent, setResponseFromContent] = useState("");
 
   useEffect(() => {
     const queryInfo = {active: true, lastFocusedWindow: true};
@@ -13,6 +16,51 @@ function App() {
     });
   }, []);
 
+  const sendTestMessage = () => {
+    const message: ChromeMessage = {
+      from: Sender.React,
+      mesage: "Hello from React",
+    }
+
+    const queryInfo: chrome.tabs.QueryInfo = {
+      active: true,
+      currentWindow: true
+    };
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const currentTabId = tabs[0].id;
+
+      chrome.tabs.sendMessage(
+        currentTabId,
+        message,
+        (response) => {
+          setResponseFromContent(response);
+        });
+    });
+  };
+  
+  const sendRemoveMessage = () => {
+    const message: ChromeMessage = {
+      from: Sender.React,
+      message: "delete logo",
+    }
+
+    const queryInfo: chrome.tabs.QueryInfo = {
+      active: true,
+      currentWindow: true
+    };
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const currentTabId = tabs[0].id;
+      chrome.tabs.sendMessage(
+        currentTabId,
+        message,
+        (response) => {
+          setResponseFromContent(response);
+        });
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +68,12 @@ function App() {
         <p>URL:</p>
         <p>
           {url}
+        </p>
+        <button onClick={sendTestMessage}>SEND MESSAGE</button>
+        <button onClick={sendRemoveMessage}>Remove logo</button>
+        <p>Response from content:</p>
+        <p>
+          {responseFromContent}
         </p>
       </header>
     </div>
