23c9d2cde6ed4fb28f14b65853db994af016f11d
Sarah Chen <scart11235@gmail.com>
2023-04-15 19:13:32 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[url, setUrl] = useState("");
+
+  useEffect(() => {
+    const queryInfo = {active: true, lastFocusedWindow: true};
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const url = "" + tabs[0].url;
+      setUrl(url);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>URL:</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {url}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
